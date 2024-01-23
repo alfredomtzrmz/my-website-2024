20cc7d3c72986cdcbd962bcd4bf3858aa04c8133
Alfredo Martinez <ialfredo.mtz.rmz@gmail.com>
2024-01-22 21:30:59 -0600

    Feat: Add ESLint and Prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// Docs: https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  plugins: [],
+  rules: {
+    'vue/no-v-html': 0,
+  },
+};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  devServer:{
-    https:{
+  devServer: {
+    https: {
       key: './certificates/alfredom.test.dev-key.pem',
-      cert: './certificates/alfredom.test.dev.pem'
+      cert: './certificates/alfredom.test.dev.pem',
     },
-    host:'alfredom.test.dev',
+    host: 'alfredom.test.dev',
   },
   modules: ['@nuxt/ui'],
-})
+});
